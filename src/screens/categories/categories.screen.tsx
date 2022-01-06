ea89b6456914fdd210220de00ba265b2d6eb0933
yayo <yayobyte@gmail.com>
2022-01-06 22:14:37 +0400

    First Screens

---
+++ src/screens/categories/categories.screen.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from "react-native";
+import {styles} from './categories.screen.styles'
+
+export const CategoriesScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Categories Screen!</Text>
+        </View>
+    )
+}
---
+++ src/screens/filter/filter.screen.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from "react-native";
+import {styles} from "../categories/categories.screen.styles";
+
+export const FilterScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Categories Screen!</Text>
+        </View>
+    )
+}
