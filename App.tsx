87b56fe358d2aec11f13a4ffce1b0fbcd2617114
yayo <yayobyte@gmail.com>
2022-01-06 22:45:15 +0400

    fonts loader and dependencies for navigation

--- App.tsx
+++ App.tsx
@@ -1,7 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { Fonts } from './src/types/general';
+import {useState} from "react";
+import AppLoading from 'expo-app-loading';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    [Fonts.openSans]: require('./assets/fonts/OpenSans-Regular.ttf'),
+    [Fonts.openSansBold]: require('./assets/fonts/OpenSans-Bold.ttf'),
+  })
+}
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if(!fontsLoaded) {
+    return (
+        <AppLoading
+          startAsync={fetchFonts}
+          onError={(err) => console.error('Error loading fonts: ', err)}
+          onFinish={() => setFontsLoaded(true)}
+        />
+    )
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
---
+++ src/types/general.ts
@@ -0,0 +1,4 @@
+export enum Fonts {
+    openSans = 'open-sans',
+    openSansBold = 'open-sans-bold',
+}
