588772bd42757a654e6c91ed113496e9af2ef5b3
yayo <yayobyte@gmail.com>
2022-01-12 00:41:01 +0400

    navigation functions

--- src/screens/category-meal/category-meal.screen.tsx
+++ src/screens/category-meal/category-meal.screen.tsx
@@ -1,10 +1,16 @@
-import {Text, View} from "react-native";
+import {Button, Text, View} from "react-native";
 import {styles} from "../categories/categories.screen.styles";
+import {NAV_MEAL_DETAIL} from "../../navigation/screen-names";
 
-export const CategoryMealScreen = () => {
+export const CategoryMealScreen = ({ navigation }: any) => {
+    const goToMealsHandler = () => { navigation.navigate({ routeName: NAV_MEAL_DETAIL }) };
+    const goBackHandler = () => { navigation.goBack() };
+
     return (
         <View style={styles.container}>
             <Text>Categories Screen!</Text>
+            <Button title={'Go to details'} onPress={goToMealsHandler} />
+            <Button title={'Go back'} onPress={goBackHandler} />
         </View>
     )
 }
--- src/screens/meal-details/meal-details.screen.tsx
+++ src/screens/meal-details/meal-details.screen.tsx
@@ -1,10 +1,13 @@
-import {Text, View} from "react-native";
+import {Button, Text, View} from "react-native";
 import {styles} from "../categories/categories.screen.styles";
 
-export const MealDetailsScreen = () => {
+export const MealDetailsScreen = ({ navigation }: any) => {
+
+    const goBackHandler = () => { navigation.popToTop() };
     return (
         <View style={styles.container}>
             <Text>Categories Screen!</Text>
+            <Button title={'Go To Start'} onPress={goBackHandler} />
         </View>
     )
 }
