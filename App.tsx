3448cb3e5899bda9c837480634da7d63658a4746
yayobyte <yayobyte@gmail.com>
2022-11-30 13:35:28 +0400

    useFonts hook

--- App.tsx
+++ App.tsx
@@ -1,27 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import * as Font from 'expo-font';
+import { useFonts } from 'expo-font';
 import { Fonts } from './src/types/general';
-import {useState} from "react";
 import AppLoading from 'expo-app-loading';
 import {MealsNavigator} from "./src/navigation";
 
-const fetchFonts = () => {
-  return Font.loadAsync({
-    [Fonts.openSans]: require('./assets/fonts/OpenSans-Regular.ttf'),
-    [Fonts.openSansBold]: require('./assets/fonts/OpenSans-Bold.ttf'),
-  })
+const fonts = {
+  [Fonts.openSans]: require('./assets/fonts/OpenSans-Regular.ttf'),
+  [Fonts.openSansBold]: require('./assets/fonts/OpenSans-Bold.ttf'),
 }
 
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
+  const [fontsLoaded] = useFonts(fonts)
 
   if(!fontsLoaded) {
     return (
-        <AppLoading
-            startAsync={fetchFonts}
-            onError={(err) => console.error('Error loading fonts: ', err)}
-            onFinish={() => setFontsLoaded(true)}
-        />
+        <AppLoading />
     )
   }
 
