e630ad057559c998a3ed2d1211f1ba0ffa3b6d3c
yayobyte <yayobyte@gmail.com>
2022-12-13 13:13:32 +0400

    create context

--- App.tsx
+++ App.tsx
@@ -1,31 +1,34 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { useFonts } from 'expo-font';
-import { Fonts } from './src/types/general';
+import {NavigationContainer} from '@react-navigation/native';
+import {StatusBar} from 'expo-status-bar';
+import {useFonts} from 'expo-font';
+import {Fonts} from './src/types/general';
 import AppLoading from 'expo-app-loading';
 import {StackNavigator} from "./src/navigation/meals-navigator";
+import {FavoritesContextProvider} from "./src/store/context/favorites.context";
 
 const fonts = {
-  [Fonts.openSans]: require('./assets/fonts/OpenSans-Regular.ttf'),
-  [Fonts.openSansBold]: require('./assets/fonts/OpenSans-Bold.ttf'),
+    [Fonts.openSans]: require('./assets/fonts/OpenSans-Regular.ttf'),
+    [Fonts.openSansBold]: require('./assets/fonts/OpenSans-Bold.ttf'),
 }
 
 export default function App() {
-  const [fontsLoaded] = useFonts(fonts)
+    const [fontsLoaded] = useFonts(fonts)
 
-  if(!fontsLoaded) {
+    if (!fontsLoaded) {
+        return (
+            <AppLoading/>
+        )
+    }
+
     return (
-        <AppLoading />
-    )
-  }
-
-  return (
-      <>
-        <StatusBar style={'dark'}/>
-        <NavigationContainer>
-            <StackNavigator />
-        </NavigationContainer>
-      </>
-);
+        <>
+            <StatusBar style={'dark'}/>
+            <FavoritesContextProvider>
+                <NavigationContainer>
+                    <StackNavigator/>
+                </NavigationContainer>
+            </FavoritesContextProvider>
+        </>
+    );
 }
 
---
+++ src/store/context/favorites.context.tsx
@@ -0,0 +1,32 @@
+import React, {createContext, useState, Context} from 'react'
+
+type FavoritesContextType = {
+    ids: Array<string>
+    addFavorite: (id: string) => void
+    removeFavorite: (id: string) => void
+}
+
+const FavoritesContext: Context<FavoritesContextType> = createContext({
+    ids: [''],
+    addFavorite: (id: string) => {},
+    removeFavorite: (id: string) => {},
+})
+
+export const FavoritesContextProvider = ({ children }: { children: React.ReactElement }) => {
+    const [favoriteMealIds, setFavoriteMealIds] = useState<Array<string>>([])
+
+    const addFavorite = (id: string) => {
+        setFavoriteMealIds((currentIds) => [...currentIds ?? [], id])
+    }
+
+    const removeFavorite = (id: string) => {
+        setFavoriteMealIds((currentIds) => currentIds?.filter((mealId) => mealId !== id))
+    }
+
+    const contextValue : FavoritesContextType = {
+        ids: favoriteMealIds,
+        addFavorite,
+        removeFavorite,
+    }
+    return <FavoritesContext.Provider value={contextValue}>{children}</FavoritesContext.Provider>
+}
